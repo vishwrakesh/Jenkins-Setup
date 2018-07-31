7f9afeca65628ddf72b5b3a92da42826ed49ff34
vishwrakesh <vishwrakesh@hotmail.com>
2018-07-31 10:51:54 +0530

    commit message change

--- src/main/java/services/SimpleService.java
+++ src/main/java/services/SimpleService.java
@@ -2,7 +2,7 @@ package services;
 
 public class SimpleService {
 	public void display() {
-		System.out.println("This is Simple service");
+		System.out.println("This is Simple service change in display text.");
 	}
 
 }
