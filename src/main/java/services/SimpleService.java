34954c1ae140f765c2fa2e83c8489854bc28a275
vishwrakesh <vishwrakesh@hotmail.com>
2018-07-31 15:21:45 +0530

    new

--- src/main/java/services/SimpleService.java
+++ src/main/java/services/SimpleService.java
@@ -2,21 +2,12 @@ package services;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 @Path("/home")
 public class SimpleService {
 
 	@GET
-	@Path("{message}")
-	public Response publishMessage(@PathParam("message") String msg) {
-		String responseStr = "Received message: "+msg;
-		return Response.status(200).entity(responseStr).build();
-	}
-	
-	@GET
-	@Path("/mypath")
 	public Response showMessage() {
 		String msg = "Welcome to deployed version";
 		return Response.status(200).entity(msg).build();
